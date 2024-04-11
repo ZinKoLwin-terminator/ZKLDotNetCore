917b18412c44028de868dc83cd29ec64377547ad
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-04-11 14:38:26 +0630

    Ado.Net Create

---
+++ ZKLDotNetCore.ConsoleApp/AdoDotNetExamples.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZKLDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExamples
+
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",//Server Name
+            InitialCatalog = "ZKLDotNetCore",//database name
+            UserID="sa",
+            Password="sasa@123",
+
+        };
+        public void Read() {
+           
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            Console.WriteLine("open");
+
+            string query = "select * from tbl_blog";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            connection.Close();
+            Console.WriteLine("close");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("Blog Id:" + dr["BlogId"]);
+                Console.WriteLine("Blog Title:" + dr["BlogTitle"]);
+
+                Console.WriteLine("Blog Author:" + dr["BlogAuthor"]);
+                Console.WriteLine("Blog Content:" + dr["BlogContent"]);
+                Console.WriteLine(".............................");
+            }
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"INSERT INTO [dbo].[tbl_blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle,
+		   @BlogAuthor,
+		   @BlogContent)";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@BlogTitle", title);
+            command.Parameters.AddWithValue("@BlogAuthor", author);
+            command.Parameters.AddWithValue("@BlogContent", content);
+            int result=command.ExecuteNonQuery();
+            connection.Close();
+            string message = result > 0 ? "Saving Successful." : "Saving Failed";
+            Console.WriteLine(message);
+        }
+    }
+
+   
+}
--- ZKLDotNetCore.ConsoleApp/Program.cs
+++ ZKLDotNetCore.ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 
 using System.Data;
 using System.Data.SqlClient;
+using ZKLDotNetCore.ConsoleApp;
 
 Console.WriteLine("Hello,World");
 //npm
@@ -15,31 +16,10 @@ Console.WriteLine("Hello,World");
 
 
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = "DESKTOP-6HBOPOG";
-stringBuilder.InitialCatalog = "ZKLDotNetCore";
-stringBuilder.UserID = "sa";
-stringBuilder.Password = "sasa@123";
-SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
-connection.Open();
-Console.WriteLine("open");
+AdoDotNetExamples adoDotNetExamples = new AdoDotNetExamples();
 
-string query = "select * from tbl_blog";
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-DataTable dt=new DataTable();
-adapter.Fill(dt);
-connection.Close();
-Console.WriteLine("close");
+adoDotNetExamples.Create("title", "author", "content");
 
-foreach (DataRow dr in dt.Rows)
-{
-    Console.WriteLine("Blog Id:" + dr["BlogId"]);
-    Console.WriteLine("Blog Title:" + dr["BlogTitle"]);
+adoDotNetExamples.Read();
 
-    Console.WriteLine("Blog Author:" + dr["BlogAuthor"]);
-    Console.WriteLine("Blog Content:" + dr["BlogContent"]);
-    Console.WriteLine(".............................");
-}
-
-Console.ReadKey();
+Console.ReadLine();
