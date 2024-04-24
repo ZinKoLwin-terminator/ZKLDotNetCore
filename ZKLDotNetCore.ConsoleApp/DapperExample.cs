5be5d5f525c577d50379d2061436967d095c1533
ZinKoLwin-terminator <zinkolwin161198@gmail.com>
2024-04-24 07:31:07 +0630

    Insert,UPdate,delete by using dapper

--- ZKLDotNetCore.ConsoleApp/DapperExample.cs
+++ ZKLDotNetCore.ConsoleApp/DapperExample.cs
@@ -14,9 +14,13 @@ namespace ZKLDotNetCore.ConsoleApp
     {
         public void Run()
         {
+
+            // Edit(1);
+            //Edit(40);
+            //Update(25,"update1 title", "update1 author", "update1 content");
+            Delete(25);
             Read();
-            Edit(1);
-            Edit(40);
+
 
         }
 
@@ -49,6 +53,74 @@ namespace ZKLDotNetCore.ConsoleApp
             Console.WriteLine(item.BlogAuthor);
             Console.WriteLine(item.BlogContent);
 
+
+        }
+        private void Create(string title,string author,string content)
+        {
+            var item = new BlogDto
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content,
+            };
+
+            string query = @"INSERT INTO [dbo].[tbl_blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle,
+		   @BlogAuthor,
+		   @BlogContent)";
+
+            using IDbConnection db = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            int result = db.Execute(query, item);
+
+            string message = result > 0 ? "Saving successfully" : "Saving Fail";
+            Console.WriteLine(message);
+
+
+        }
+
+        private void Update(int id,string title, string author, string content)
+        {
+            var item = new BlogDto
+            {   BlogId= id, 
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content,
+            };
+
+            string query = @"UPDATE [dbo].[tbl_blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId=@BlogId";
+
+            using IDbConnection db = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            int result = db.Execute(query, item);
+
+            string message = result > 0 ? "Update successfully" : "Update Fail";
+            Console.WriteLine(message);
+
+
+        }
+        private void Delete(int id)
+        {
+            var item=new BlogDto { BlogId= id };
+
+            string query = @"DELETE FROM [dbo].[tbl_blog]
+      WHERE BlogId=@BlogId";
+
+            using IDbConnection db = new SqlConnection(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+
+            int result = db.Execute(query, item);
+
+
+            string message = result > 0 ? "Delete successfully" : "Delete Fail";
+            Console.WriteLine(message);
 
         }
     }
